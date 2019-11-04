f21244a230c5a83cb4c128d2aaff6d5b27e2c0eb
RyanGelow <exisphere@gmail.com>
2019-11-04 13:15:15 -0800

    added new js

---
+++ assets/js/work.js
@@ -0,0 +1,68 @@
+workSamples = [
+    {
+        img: "./assets/images/battle.gif",
+        title: "Battle Game",
+        explore: "https://github.com/RyanGelow/Battle-Game",
+        play: "https://ryangelow.github.io/Battle-Game/" 
+    },
+    {
+        img: "./assets/images/drinks.gif", 
+        title: "A Drink For Your Mood",
+        explore: "https://github.com/RyanGelow/Drink-For-Your-Mood",
+        play: "https://ryangelow.github.io/Drink-For-Your-Mood/"
+    },
+    {
+        img: "./assets/images/warriorspast.jpg", 
+        title: "Warriors Timed Trivia",
+        explore: "https://github.com/RyanGelow/Trivia-Game",
+        play: "https://ryangelow.github.io/Trivia-Game/"
+    },
+    {
+        img: "./assets/images/giphytrending.gif", 
+        title: "Giphy Action Page",
+        explore: "https://github.com/RyanGelow/Giphy-API-Page",
+        play: "https://ryangelow.github.io/Giphy-API-Page/"
+    },
+    {
+        img: "./assets/images/lirigiphy.gif", 
+        title: "Liri Bot",
+        explore: "https://github.com/RyanGelow/liri-node-app/",
+        readme: "https://ryangelow.github.io/liri-node-app/"
+    },
+    {
+        img: "./assets/images/marketplacegiphy.gif", 
+        title: "Bamazon Marketplace",
+        explore: "https://github.com/RyanGelow/bamazon",
+        play: "https://ryangelow.github.io/bamazon/"
+    },
+    {
+        img: "./assets/images/bestfriendsgif.gif", 
+        title: "Friend Finder",
+        explore: "https://github.com/RyanGelow/Friend-Finder",
+        play: "https://friend-finder-reg.herokuapp.com/"
+    },
+    {
+        img: "./assets/images/sportsgiphy.gif", 
+        title: "Sports News Web Scraper",
+        explore: "https://ryangelow.github.io/Scraper-App/",
+        play: "https://scraper-app-ryan.herokuapp.com/"
+    },
+    {
+        img: "./assets/images/fortuneteller.gif", 
+        title: "Memory Game",
+        explore: "https://ryangelow.github.io/Memory-Game/",
+        play: "https://enigmatic-island-89427.herokuapp.com/"
+    },
+    {
+        img: "./assets/images/mathzachg.gif", 
+        title: "The Deals",
+        explore: "https://github.com/RyanGelow/Project2-TheDeals/",
+        play: "https://the-deals-project2.herokuapp.com/"
+    },
+    {
+        img: "./assets/images/giphytreneatingding.gif", 
+        title: "Meal Planner",
+        explore: "https://github.com/RyanGelow/Meal-Planner",
+        readme: "https://ryangelow.github.io/Meal-Planner/"
+    }
+]
